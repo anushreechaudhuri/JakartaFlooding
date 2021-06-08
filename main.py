6b35bdc636ac9006a4b4f1c6545843be20b97221
Anushree Chaudhuri <anuc@mit.edu>
2021-06-08 08:39:07 -0700

    added maps for 2013, working on script for all years

--- main.py
+++ main.py
@@ -4,26 +4,25 @@ import os
 
 opj = os.path.join
 
-stuff_path = r'C:\Users\anush\PycharmProjects\JakartaFlooding\stuff'
-aprx = arcpy.mp.ArcGISProject(r'C:\Users\anush\PycharmProjects\JakartaFlooding\stuff\emptyproject.aprx')
+output_path = r'C:\Users\anush\PycharmProjects\JakartaFlooding\output'
+aprx = arcpy.mp.ArcGISProject(r'C:\Users\anush\PycharmProjects\JakartaFlooding\output\emptyproject.aprx')
 m = aprx.listMaps("Map")[0]
-m.addDataFromPath(r'C:\Users\anush\Downloads\BANJIR TAHUN 2013\BANJIR TAHUN 2013.shp')
+years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
+year = '2013'
+
+m.addDataFromPath(r'C:\Users\anush\Downloads\BANJIR TAHUN 2013 - 2020\BANJIR TAHUN 2013 - 2020\BANJIR TAHUN 2013.shp')
 l = m.listLayers('*')[0]
 
 sym = l.symbology
 
-
-
-# print('\n\n\n\n'.join([x.name for x in aprx.listColorRamps('*')]))
-# if hasattr(sym, 'renderer'):
-#   if sym.renderer.type == 'SimpleRenderer':
 sym.updateRenderer('GraduatedColorsRenderer')
-sym.renderer.classificationField = 'JAN_2013'
-sym.renderer.breakCount = 4
-sym.renderer.colorRamp = aprx.listColorRamps('Blues (Continuous)')[0]
+months = [f'JAN_{year}', f'FEB_{year}', f'MAR_{year}', f'APRIL_{year}', f'MEI_{year}', f'JUN_{year}',
+          f'JUL_{year}', f'AGU_{year}', f'OKT_{year}', f'NOV_{year}', f'DES_{year}']
+for month in months:
+    sym.renderer.classificationField = month
+    sym.renderer.breakCount = 4
+    sym.renderer.colorRamp = aprx.listColorRamps('Blues (Continuous)')[0]
 
-l.symbology = sym
-
-
-mv = m.defaultView
-mv.exportToPNG(opj(stuff_path, 'out.png'), width=1000, height=1000, world_file=True, color_mode="32-BIT_WITH_ALPHA")
+    l.symbology = sym
+    mv = m.defaultView
+    mv.exportToPNG(opj(output_path, f'map_{month}.png'), width=1000, height=1000, world_file=True, color_mode="32-BIT_WITH_ALPHA")
